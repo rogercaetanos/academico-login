42b9e994935ed5e36b8ffff9c20270ea0086ec76
Rogério Caetano <rogercaetanos@gmail.com>
2023-08-31 22:41:49 -0300

    Configuração do security parcialmente concluída

--- src/main/java/com/itb/mif3an/academicologin/service/UserService.java
+++ src/main/java/com/itb/mif3an/academicologin/service/UserService.java
@@ -1,9 +1,11 @@
 package com.itb.mif3an.academicologin.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.itb.mif3an.academicologin.model.User;
 import com.itb.mif3an.academicologin.web.dto.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService{
 	
 	User save(UserDto userDto);
 	User findByEmail(String email);
--- src/main/java/com/itb/mif3an/academicologin/service/UserServiceImpl.java
+++ src/main/java/com/itb/mif3an/academicologin/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
  package com.itb.mif3an.academicologin.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.itb.mif3an.academicologin.model.User;
@@ -28,6 +30,12 @@ public class UserServiceImpl implements UserService {
 		
 		
 		return userRepository.save(user);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
